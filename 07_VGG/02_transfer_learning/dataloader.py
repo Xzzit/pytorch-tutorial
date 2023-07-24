8afa8148d7e0836a15e157b1eca41c80262b380b
Xiang Zhizheng <50263991+Xzzit@users.noreply.github.com>
2023-07-25 00:04:30 +0900

    upload vgg transfer learning files

---
+++ 07_VGG/02_transfer_learning/dataloader.py
@@ -0,0 +1,44 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# define the dataloader
+transform = transforms.Compose(
+    [transforms.Resize(224),
+     transforms.ToTensor(),
+     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+
+batch_size = 16
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+if __name__ == '__main__':
+    # get some random training images
+    dataiter = iter(train_loader)
+    images, labels = next(dataiter)
+
+    # print labels
+    print(' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
+
+    # show images
+    img_grid = torchvision.utils.make_grid(images)
+    img_grid = img_grid / 2 + 0.5
+    npimg = img_grid.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
---
+++ 07_VGG/02_transfer_learning/model.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+# load pretrained vgg16 model
+vgg16 = models.vgg16(weights='IMAGENET1K_V1')
+
+# freeze the parameters
+for param in vgg16.parameters():
+    param.requires_grad = False
+
+# replace the last fully-connected layer
+vgg16.classifier[6] = nn.Linear(4096, 10)
+
+# test the model
+if __name__ == '__main__':
+
+    # test the model on a random tensor
+    img = torch.randn(1, 3, 224, 224)
+    output = vgg16(img)
+    print(output.shape)
