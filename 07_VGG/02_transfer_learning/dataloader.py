b238f59f7fa3f6f2a28b0753717d1456aa09fece
xzzit <897177017@qq.com>
2023-07-31 13:35:12 +0900

    increase batch size 16->32

--- 07_VGG/02_transfer_learning/dataloader.py
+++ 07_VGG/02_transfer_learning/dataloader.py
@@ -12,7 +12,7 @@ transform = transforms.Compose(
      transforms.ToTensor(),
      transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
 
-batch_size = 16
+batch_size = 32
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
