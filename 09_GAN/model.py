55847b53838d07afbfa0867858590644d751cf28
Xiang Zhizheng <50263991+Xzzit@users.noreply.github.com>
2023-07-31 18:27:13 +0900

    add discriminator and test phase

--- 09_GAN/model.py
+++ 09_GAN/model.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+
+# define the generator
 class Generator(nn.Module):
     def __init__(self, latent_dim=100, img_size=28):
         super(Generator, self).__init__()
@@ -24,3 +26,57 @@ class Generator(nn.Module):
             nn.Linear(1024, self.img_size**2),
             nn.Tanh()
         )
+
+    def forward(self, x):
+        img = self.model(x)
+        img = img.view(img.size(0), 1, self.img_size, self.img_size)
+        return img
+    
+
+# define the discriminator
+class Discriminator(nn.Module):
+    def __init__(self, img_size=28):
+        super(Discriminator, self).__init__()
+
+        # define the model architecture
+        self.model = nn.Sequential(
+            nn.Linear(img_size**2, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+    
+    def forward(self, img):
+        img_flat = img.view(img.size(0), -1)
+        pred = self.model(img_flat)
+        return pred
+
+
+# test the model
+if __name__ == '__main__':
+    # define the latent dimension
+    latent_dim = 100
+
+    # define the image size
+    img_size = 28
+
+    # create the generator
+    gen = Generator(latent_dim, img_size)
+
+    # create the discriminator
+    disc = Discriminator(img_size)
+
+    # generate a random noise
+    noise = torch.randn(3, latent_dim)
+
+    # generate a image
+    fake_img = gen(noise)
+    print(f'Generated image shape: {fake_img.shape}')
+
+    # predict the image
+    pred = disc(fake_img)
+
+    # print the result
+    print(f'Prediction: {pred}')
